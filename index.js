6f0550604b102142a240dbcfa1820c1fab38dd49
Busolami <oluwabibusola@gmail.com>
2023-02-20 15:48:43 +0100

    operatorr

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-let ghsDeptSubject = 'None Sure';
+// Question 3
+
+let ghsDeptSubject = 'Applied Science';
 if (ghsDeptSubject === 'Applied Science') {
-    console.log("Physics, Chemistry, Mathematics, English, Biology, Technical Drawing,Geography,Futher-mathematics, Civic-Education, Cateering-Craft, Computer-Studies")
+    console.log("These are the courses of Applied Science;" + " " + "Physics, Chemistry, Mathematics, English, Biology, Technical Drawing,Geography,Futher-mathematics, Civic-Education, Cateering-Craft, Computer-Studies")
 } else if (ghsDeptSubject === 'Pure Science') {
-    console.log("Physics, Mathematics, English, Chemistry, Biology, Civic-Education, Economic, Agricultural-Science, Geography, Food and Nutriction, Computer-Studies")
+    console.log("These are the courses of Pure Science;" + " " + "Physics, Mathematics, English, Chemistry, Biology, Civic-Education, Economic, Agricultural-Science, Geography, Food and Nutriction, Computer-Studies")
 } else if (ghsDeptSubject === 'Art') {
-    console.log("Mathematics, English, Biology, Civi-Education, Government, Economics, Literature, History, Animal-Husbandry, Marketing")
+    console.log("These are the courses of Art;" + " " + "Mathematics, English, Biology, Civi-Education, Government, Economics, Literature, History, Animal-Husbandry, Marketing")
 } else if (ghsDeptSubject === 'Social Science') {
-    console.log("Mathematics, English, Biology, Civi-Education, Accounting, Commerce, Marketing, Economics,Government")
+    console.log("These are the courses of Social Science;" + " " + "Mathematics, English, Biology, Civi-Education, Accounting, Commerce, Marketing, Economics,Government")
 } else {
     console.log("Mathematics, English, Biology, Civi-Education, (Trade-Subject; Cateering-Craft/Animal-Husbandry/Food-and_nutriction/Marketing)")
 }
+
+
+//Question 5
+let num = 20;
+
+2 ** 3
+2 ** 4
+
+let pwr = 2 ** 4
+console.log("The number" + " " + pwr + " " + "is the power of 2 nearest to" + " " + num);
