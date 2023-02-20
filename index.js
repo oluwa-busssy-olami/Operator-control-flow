0a97c3acf198adb3249fcdb70a3579f8908300b8
Busolami <oluwabibusola@gmail.com>
2023-02-20 14:50:11 +0100

    operator

---
+++ index.js
@@ -0,0 +1,12 @@
+let ghsDeptSubject = 'None Sure';
+if (ghsDeptSubject === 'Applied Science') {
+    console.log("Physics, Chemistry, Mathematics, English, Biology, Technical Drawing,Geography,Futher-mathematics, Civic-Education, Cateering-Craft, Computer-Studies")
+} else if (ghsDeptSubject === 'Pure Science') {
+    console.log("Physics, Mathematics, English, Chemistry, Biology, Civic-Education, Economic, Agricultural-Science, Geography, Food and Nutriction, Computer-Studies")
+} else if (ghsDeptSubject === 'Art') {
+    console.log("Mathematics, English, Biology, Civi-Education, Government, Economics, Literature, History, Animal-Husbandry, Marketing")
+} else if (ghsDeptSubject === 'Social Science') {
+    console.log("Mathematics, English, Biology, Civi-Education, Accounting, Commerce, Marketing, Economics,Government")
+} else {
+    console.log("Mathematics, English, Biology, Civi-Education, (Trade-Subject; Cateering-Craft/Animal-Husbandry/Food-and_nutriction/Marketing)")
+}
